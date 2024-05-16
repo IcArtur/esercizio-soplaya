8bb07e05a0a30c2d2c025fb83a56d4708868bdb2
Francesco Perilli <francesco.perilli@conio.it>
2024-05-16 14:48:20 +0200

    Implement sorting for each available field in the restaurant data list view.

--- soplaya_exercise/api/models.py
+++ soplaya_exercise/api/models.py
@@ -1,3 +1,5 @@
+import typing
+
 from django.db import models
 
 
@@ -11,5 +13,9 @@ class RestaurantData(models.Model):
     sells = models.FloatField()
     money_difference = models.FloatField()
 
-    def __repr__(self):
+    @classmethod
+    def get_model_fields(cls) -> typing.List[str]:
+        return [x.name for x in cls._meta.fields]
+
+    def __repr__(self) -> str:
         return f"{self.restaurant_name} - {self.date}"
--- soplaya_exercise/api/views.py
+++ soplaya_exercise/api/views.py
@@ -12,8 +12,14 @@ class RestaurantDataList(generics.ListAPIView):
         queryset = RestaurantData.objects.all()
         start_date = self.request.query_params.get('start_date')
         end_date = self.request.query_params.get('end_date')
-        if start_date is not None:
+        order_by = self.request.query_params.get('order_by')
+        sort_order = self.request.query_params.get('sort_order')
+        if start_date:
             queryset = queryset.filter(date__gte=start_date)
-        if end_date is not None:
+        if end_date:
             queryset = queryset.filter(date__lte=end_date)
+        if order_by is not None and order_by.lower() in RestaurantData.get_model_fields():
+            sort_prefix = '-' if sort_order.lower() == 'asc' else ''
+            order_by = sort_prefix + order_by
+            queryset = queryset.order_by(order_by)
         return queryset
