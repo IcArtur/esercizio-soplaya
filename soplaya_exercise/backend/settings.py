561d24c58d513ecf44a87909ad66c8791036647a
Francesco Perilli <francesco.perilli@conio.it>
2024-05-16 14:02:44 +0200

    Rename folder to backend.

--- soplaya_exercise/backend/settings.py
+++ soplaya_exercise/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7m7i-(ia9zo4(x^&k)(^n2i-!v%f9h&@s-i6r6-pfw!b_+x*eo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'soplaya_exercise.urls'
+ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'soplaya_exercise.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 # Database
--- soplaya_exercise/backend/wsgi.py
+++ soplaya_exercise/backend/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soplaya_exercise.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
