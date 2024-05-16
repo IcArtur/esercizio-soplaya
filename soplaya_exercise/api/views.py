69cb369eb96965744dc433f00b8ad300f079ea99
Francesco Perilli <francesco.perilli@conio.it>
2024-05-16 14:28:35 +0200

    Add range date filter to the restaurant data list view.

--- soplaya_exercise/api/views.py
+++ soplaya_exercise/api/views.py
@@ -6,5 +6,14 @@ from .serializers import RestaurantDataSerializer
 
 
 class RestaurantDataList(generics.ListAPIView):
-    queryset = RestaurantData.objects.all()
     serializer_class = RestaurantDataSerializer
+
+    def get_queryset(self):
+        queryset = RestaurantData.objects.all()
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        if start_date is not None:
+            queryset = queryset.filter(date__gte=start_date)
+        if end_date is not None:
+            queryset = queryset.filter(date__lte=end_date)
+        return queryset
