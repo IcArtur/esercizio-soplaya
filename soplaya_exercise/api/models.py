c70d9c500cae64788ae8a0e88d9569e364a01e44
Francesco Perilli <francesco.perilli@conio.it>
2024-05-16 12:25:07 +0200

    Django base configuration with apps.

---
+++ soplaya_exercise/api/models.py
@@ -0,0 +1,13 @@
+import decimal
+
+from django.db import models
+
+class RestaurantData(models.Model):
+    restaurant_name = models.CharField(max_length=100)
+    date = models.DateField()
+    planned_hours = models.IntegerField()
+    actual_hours = models.IntegerField()
+    hours_difference = models.IntegerField()
+    budget = models.FloatField()
+    sells = models.FloatField()
+    money_difference = models.FloatField()
